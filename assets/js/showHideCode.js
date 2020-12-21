ce853b3ada2ab7aac1c2c391c2600e5dd190bbed
SirJamie <jamie98@vip.qq.com>
2020-12-21 23:30:01 +0800

    add showHideCode in frontpage

--- assets/js/showHideCode.js
+++
@@ -1,8 +0,0 @@
-function change(){
-    var obj=document.getElementById("showdiv");
-    if(obj.style.display=="none"){
-    obj.style.display="block"
-    }else{
-    obj.style.display="none"
-    }
-}
